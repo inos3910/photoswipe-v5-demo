94de849fc8f896a4df10168a3a150671abef54b2
Yosuke Inoue <ysk.inoue@sharesl.net>
2022-10-13 17:34:04 +0900

    [fix] JS 画像のwidth・height取得コードの修正

--- assets/js/common/entry.js
+++ assets/js/common/entry.js
@@ -1,36 +1,40 @@
-import imagesLoaded from 'imagesloaded'
 import PhotoSwipeLightbox from 'photoswipe/lightbox';
 import PhotoSwipe from 'photoswipe';
 
 class Gallery {
   constructor() {
-    this.imagesloaded();
+    this.imagePopUp();
   }
 
-  imagesloaded(){
-    const gallery = document.querySelector('[data-pswp]');
-    if(!gallery){
-      return;
-    }
-
-    imagesLoaded(gallery, () => {
-      gallery.classList.remove('is-loading');
-      this.imagePopUp(gallery);
+  //特定の画像の読み込み
+  loadImage(src) {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = (e) => reject(e);
+      img.src = src;
     });
   }
 
   // スライド画像のポップアップ（PhotoSwipe）
-  imagePopUp(gallery){
+  imagePopUp(){
+    const gallery = document.querySelector('#js-gallery');
     if(!gallery){
       return;
     }
 
     const galleries = gallery.querySelectorAll('a');
+    galleries.forEach( async (el, index, array) => {
+      // this.loadImage(el.href).then( img => {
+      //   el.setAttribute('data-pswp-width', img.naturalWidth);
+      //   el.setAttribute('data-pswp-height', img.naturalHeight);
+      //   el.classList.remove('is-loading');
+      // });
 
-    galleries.forEach((el) => {
-      const img = el.querySelector('img');
+      const img = await this.loadImage(el.href);
       el.setAttribute('data-pswp-width', img.naturalWidth);
       el.setAttribute('data-pswp-height', img.naturalHeight);
+      el.classList.remove('is-loading');
     });
 
     const lightbox = new PhotoSwipeLightbox({
